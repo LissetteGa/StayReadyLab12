a6934d412797a1d6ab223a4fff4ab205cda23e7c
Lissette Garcia <lissettegarcia@Lissettes-MacBook-Air.local>
2020-07-24 18:21:51 -0500

    Done

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,41 @@
 package io.codedifferently;
 
+import java.util.Stack;
+
 public class OpenCloseChecker {
+    
+    public static void main(String[] args) {
+
+        String str = "a(b)c{d}";
+        System.out.println(OpenCloseChecker.Parentheses(str));
+    }
+
+    public static boolean Parentheses(String input) {
+        Stack<Character> sentence = new Stack<Character>();
+
+        for(int i = 0; i < input.length();i++) {
+            char parenth = input.charAt(i);
+            
+            if(parenth == '(' || parenth == '{' || parenth == '[' || parenth == '<' || parenth == '"') {
+                sentence.peek();
+            } 
+            if(parenth == ')' || parenth == ']' || parenth == '}' || parenth == '>' || parenth == '"') {
+				if (sentence.isEmpty()) {
+					return false;
+                } else if(sentence.peek() == '(' && parenth ==')') {
+                    sentence.pop();
+                } else if(sentence.peek() == '{' && parenth =='}') {
+                    sentence.pop();
+                } else if(sentence.peek() == '[' && parenth ==']') {
+                    sentence.pop();
+                } else if(sentence.peek() == '<' && parenth =='>') {
+                    sentence.pop();
+                } else if(sentence.peek() == '"' && parenth =='"') {
+                    sentence.pop();
+                }
+            }
+        }
+        return sentence.isEmpty();
+    }
+
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,69 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class OpenCloseCheckerTest {
+
+    OpenCloseChecker openC;
+
+    @Before
+    public void initialize() {
+        openC = new OpenCloseChecker();
+    }
+
+    @Test
+    public void test1(){
+        //Given
+        String sentence = "a(b)c(d)e(f)g";
+        boolean expected = true;
+        
+        //When
+        boolean actual = OpenCloseChecker.Parentheses(sentence);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2(){
+        //Given
+        String sentence = ")";
+        boolean expected = false;
+        
+        //When
+        boolean actual = OpenCloseChecker.Parentheses(sentence);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+        //Given
+        String sentence = "a(bcd(ef))";
+        boolean expected = true;
+        
+        //When
+        boolean actual = OpenCloseChecker.Parentheses(sentence);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test4(){
+        //Given
+        String sentence = "a(bc(d)))";
+        boolean expected = false;
+        
+        //When
+        boolean actual = OpenCloseChecker.Parentheses(sentence);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 
 }
